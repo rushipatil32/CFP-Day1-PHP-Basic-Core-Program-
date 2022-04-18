b3426a5622e3b4534c3ecdcfb939cc6e0c3867c7
Rushikesh Patil <rushipatil6632@gmail.com>
2022-04-18 22:04:01 +0530

    Added:To check quotient and reminder

---
+++ Basic Core Programs/QuotientReminder.php
@@ -0,0 +1,8 @@
+<?php
+$dividend = readline('Enter the dividend number : ');
+$divisor = readline('Enter the divisor number : ');
+$quotient = $dividend / $divisor;
+echo "Quotient is $quotient";
+$remainder = $dividend % $divisor;
+echo "Remainder is $remainder";
+?>
