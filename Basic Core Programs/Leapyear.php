459534122fa868af1ab8de452bc31525a398bfd3
Rushikesh Patil <rushipatil6632@gmail.com>
2022-04-18 21:38:48 +0530

    Added:Check the year is leap year or not

---
+++ Basic Core Programs/Leapyear.php
@@ -0,0 +1,11 @@
+<?php
+$Year = readline("Enter a Year");
+if ($Year%400==0 and $Year%100 !=0) {
+    echo "$Year is a leap year";
+}
+else if ($Year%4==0){
+    echo "$Year is a leap year";
+} else{
+    echo "$Year is not a leap year";
+}
+?>
